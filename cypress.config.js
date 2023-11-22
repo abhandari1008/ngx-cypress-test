17129cac334f8d9fd7a5ca41c9e2ec5b5d809491
abhandari1008 <151738148+abhandari1008@users.noreply.github.com>
2023-11-22 12:47:14 +0000

    cypress-framework

---
+++ cypress.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require('cypress')
+
+
+module.exports = defineConfig({
+  viewportHeight: 1080, 
+  viewportWidth: 1920,
+  video: false,
+  e2e: {
+    baseUrl: 'https://leaseflowdemouat.click2finance.co.uk/',
+    excludeSpecPattern:  ["**/2-advanced-examples/*", "**/1-getting-started/*"],
+    specPattern: 	'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
